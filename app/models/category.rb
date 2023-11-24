47ceaa1d1f3fd793bfd1c4a56577bd8fc9866452
pierreAngeloAA <131717275+pierreAngeloAA@users.noreply.github.com>
2023-11-24 08:37:53 -0500

    Implementation of the category and subcategory model

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ApplicationRecord
+    belongs_to :parent, class_name: 'Category', optional: true
+    has_many :subcategories, class_name: 'Category', foreign_key: 'parent_id', dependent: :destroy
+  
+    validates :name, presence: true
+end
+  
